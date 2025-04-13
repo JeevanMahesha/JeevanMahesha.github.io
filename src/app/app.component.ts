3c4364a68e6b777e02dc1d6e6716338288560d9d
Jeevan Mahesha <csejeevanm@gmail.com>
2025-04-13 14:14:21 +0530

    feat: include AboutComponent and FooterComponent in AppComponent template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,25 @@
 import { Component } from '@angular/core';
 import { NavigationComponent } from './navigation/navigation.component';
 import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavigationComponent, HeroComponent],
+  imports: [
+    NavigationComponent,
+    HeroComponent,
+    AboutComponent,
+    FooterComponent,
+  ],
   template: `
     <div className="min-h-screen bg-gray-50">
       <JM-navigation></JM-navigation>
       <main>
         <JM-hero></JM-hero>
+        <JM-about></JM-about>
       </main>
+      <JM-footer></JM-footer>
     </div>
   `,
 })
