6fc7bcdd6990fcded8e0fe3ceee3bd5e4ceced27
Jeevan Mahesha <csejeevanm@gmail.com>
2025-05-31 18:11:05 +0530

    refactor: update to zoneless change detection in app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,9 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter([]),
-  ],
+  providers: [provideZonelessChangeDetection(), provideRouter([])],
 };
