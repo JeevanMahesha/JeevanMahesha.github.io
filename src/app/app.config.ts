63fc206a33e4ca82fa42143224a0ffacaa3ee8f0
Jeevan Mahesha <csejeevanm@gmail.com>
2025-05-31 18:33:17 +0530

    feat: implement social media profiles service and update footer and hero components

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,8 @@ import {
   provideZonelessChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { SocialMedia } from './shared/services/social-media';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZonelessChangeDetection(), provideRouter([])],
+  providers: [provideZonelessChangeDetection(), provideRouter([]), SocialMedia],
 };
--- src/app/hero/hero.ts
+++ src/app/hero/hero.ts
@@ -1,10 +1,13 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
 import {
-  GithubIcon,
-  LinkedinIcon,
-  LucideAngularModule,
-  TwitterIcon,
-} from 'lucide-angular';
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  Signal,
+} from '@angular/core';
+import { LucideAngularModule } from 'lucide-angular';
+import { SocialMedia } from '../shared/services/social-media';
+import { SocialMediaProfile } from '../shared/services/social-media.model';
 
 @Component({
   selector: 'JM-hero',
@@ -13,9 +16,14 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeroComponent {
-  icons = {
-    github: GithubIcon,
-    linkedin: LinkedinIcon,
-    twitter: TwitterIcon,
-  };
+  socialMediaProfiles: Signal<SocialMediaProfile[]>;
+
+  constructor() {
+    const socialMediaService = inject(SocialMedia);
+    this.socialMediaProfiles = computed(() =>
+      socialMediaService
+        .socialMediaProfiles()
+        .filter((profile) => profile.name !== 'mail')
+    );
+  }
 }
