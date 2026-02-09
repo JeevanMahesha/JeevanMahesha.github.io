781d72759a7d0bf4887037a28d72e686400acb50
Jeevan Mahesha <csejeevanm@gmail.com>
2026-02-09 21:07:45 +0530

    refactor: dynamically render skill icons using Semantic Icons components

--- src/app/skills/skills.ts
+++ src/app/skills/skills.ts
@@ -1,65 +1,136 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Type,
+  viewChild,
+  ViewContainerRef,
+} from '@angular/core';
+import {
+  SiAngularIcon,
+  SiBootstrapIcon,
+  SiCssIcon,
+  SiDjangoIcon,
+  SiDockerIcon,
+  SiEsbuildIcon,
+  SiExpressIcon,
+  SiFastapiIcon,
+  SiFirebaseIcon,
+  SiFlaskIcon,
+  SiGithubIcon,
+  SiGitIcon,
+  SiGraphqlIcon,
+  SiHtml5Icon,
+  SiJavascriptIcon,
+  SiJestIcon,
+  SiMongodbIcon,
+  SiNestjsIcon,
+  SiNginxIcon,
+  SiNpmIcon,
+  SiPnpmIcon,
+  SiPostgresqlIcon,
+  SiPwaIcon,
+  SiPythonIcon,
+  SiReactIcon,
+  SiReactivexIcon,
+  SiReduxIcon,
+  SiRustIcon,
+  SiSassIcon,
+  SiSpringIcon,
+  SiTypescriptIcon,
+  SiWebpackIcon,
+  SiYarnIcon,
+} from '@semantic-icons/simple-icons';
+
+const CATEGORY = {
+  frontend: 'Frontend',
+  backend: 'Backend',
+  tools: 'Tools & DevOps',
+};
 
 @Component({
   selector: 'JM-skills',
   templateUrl: './skills.html',
+  imports: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Skills {
-  skills = [
+  frontendRef = viewChild('Frontend', { read: ViewContainerRef });
+  backendRef = viewChild('Backend', { read: ViewContainerRef });
+  toolsRef = viewChild('Tools', { read: ViewContainerRef });
+  categoryValue = CATEGORY;
+
+  skills: Array<{
+    category: string;
+    icons: Type<unknown>[];
+  }> = [
     {
       category: 'Frontend',
-      items: [
-        'HTML5',
-        'CSS',
-        'JavaScript',
-        'TypeScript',
-        'Angular',
-        'React',
-        'Next.js',
-        'TailwindCSS',
-        'Sass',
-        'Bootstrap',
-        'reactivex',
-        'Redux',
-        'pwa',
-        'ngrx',
-        'jest',
+      icons: [
+        SiHtml5Icon,
+        SiCssIcon,
+        SiJavascriptIcon,
+        SiTypescriptIcon,
+        SiAngularIcon,
+        SiReactIcon,
+        SiSassIcon,
+        SiBootstrapIcon,
+        SiReactivexIcon,
+        SiReduxIcon,
+        SiPwaIcon,
+        SiJestIcon,
       ],
     },
     {
       category: 'Backend',
-      items: [
-        'Node.js',
-        'Express',
-        'PostgreSQL',
-        'GraphQL',
-        'nestjs',
-        'rust',
-        'Django',
-        'Flask',
-        'Spring',
-        'FastAPI',
-        'MongoDB',
-        'Firebase',
-        'python',
+      icons: [
+        SiExpressIcon,
+        SiPostgresqlIcon,
+        SiGraphqlIcon,
+        SiNestjsIcon,
+        SiRustIcon,
+        SiDjangoIcon,
+        SiFlaskIcon,
+        SiSpringIcon,
+        SiFastapiIcon,
+        SiMongodbIcon,
+        SiFirebaseIcon,
+        SiPythonIcon,
       ],
     },
     {
       category: 'Tools & DevOps',
-      items: [
-        'Git',
-        'Docker',
-        'esbuild',
-        'github',
-        'githubpages',
-        'githubactions',
-        'npm',
-        'pnpm',
-        'webpack',
-        'yarn',
-        'nginx',
+      icons: [
+        SiGitIcon,
+        SiDockerIcon,
+        SiEsbuildIcon,
+        SiGithubIcon,
+        SiNpmIcon,
+        SiPnpmIcon,
+        SiWebpackIcon,
+        SiYarnIcon,
+        SiNginxIcon,
       ],
     },
   ];
+
+  ngAfterViewInit() {
+    this.skills.forEach((skillSet) => {
+      skillSet.icons.forEach((icon) => {
+        switch (skillSet.category) {
+          case CATEGORY.frontend:
+            const frontendRef = this.frontendRef()?.createComponent(icon);
+            frontendRef?.location.nativeElement.classList.add('size-8');
+            break;
+          case CATEGORY.backend:
+            const backendRef = this.backendRef()?.createComponent(icon);
+            backendRef?.location.nativeElement.classList.add('size-8');
+            break;
+          case CATEGORY.tools:
+            const toolsRef = this.toolsRef()?.createComponent(icon);
+            toolsRef?.location.nativeElement.classList.add('size-8');
+            break;
+        }
+      });
+    });
+  }
 }
