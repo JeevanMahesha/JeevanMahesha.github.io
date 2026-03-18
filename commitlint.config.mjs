2f09036a062f37263a48496a9524a21067d243ac
Jeevan Mahesha <csejeevanm@gmail.com>
2026-03-18 23:10:11 +0530

    chore: update commitlint configuration to enforce body line length
    - Added a rule to limit the body line length to 150 characters in commitlint.config.mjs.

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,6 +1,8 @@
+/* eslint-disable import/no-anonymous-default-export */
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'header-max-length': [2, 'always', 150],
+    'body-max-line-length': [2, 'always', 150],
   },
 };
