5032f2f30366c3f6dc4a5b64e9807dd15bd0d9a9
Jeevan Mahesha <csejeevanm@gmail.com>
2026-03-18 22:43:03 +0530

    chore: migrate project from Angular to Next.js, update dependencies, and restructure configuration files

--- src/app/shared/services/social-media.model.ts
+++
@@ -1,9 +0,0 @@
-import { LucideIconData } from 'lucide-angular';
-
-export type SocialMediaName = 'github' | 'linkedin' | 'twitter' | 'mail';
-export interface SocialMediaProfile {
-  name: SocialMediaName;
-  url: string;
-  icon: LucideIconData;
-  ariaLabel: string;
-}
