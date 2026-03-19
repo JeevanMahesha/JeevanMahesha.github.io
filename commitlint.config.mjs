c8453ea38988fa7eda0588f244a3285a91a84497
Jeevan Mahesha <csejeevanm@gmail.com>
2026-03-19 23:03:03 +0530

    chore: standardize configuration files and improve semantic-release setup
    - Updated formatting in commitlint.config.mjs, release.config.mjs, tailwind.config.ts for consistency.
    - Ensured all strings use double quotes for uniformity across configuration files.
    - Maintained existing functionality while enhancing readability and maintainability of the configuration files.

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,8 +1,8 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
   rules: {
-    'header-max-length': [2, 'always', 150],
-    'body-max-line-length': [2, 'always', 150],
+    "header-max-length": [2, "always", 150],
+    "body-max-line-length": [2, "always", 150],
   },
 };
