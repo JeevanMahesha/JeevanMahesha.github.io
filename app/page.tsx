a1847aaa415d4d2d9f11a268c4d52a777a067369
Jeevan Mahesha <csejeevanm@gmail.com>
2026-03-22 15:23:58 +0530

    refactor: implement dynamic imports for components in page.tsx
    - Replaced static imports with dynamic imports to improve performance and reduce initial load time.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
-import { Navbar } from "./components/navbar";
-import { Hero } from "./components/hero";
-import { AboutSection } from "./components/about-section";
-import { Toolbox } from "./components/toolbox";
-import { ContactSection } from "./components/contact-section";
-import { Footer } from "./components/footer";
+import dynamic from "next/dynamic";
 
+const Navbar = dynamic(() =>
+  import("./components/navbar").then((mod) => mod.Navbar),
+);
+const Hero = dynamic(() => import("./components/hero").then((mod) => mod.Hero));
+const AboutSection = dynamic(() =>
+  import("./components/about-section").then((mod) => mod.AboutSection),
+);
+const Toolbox = dynamic(() =>
+  import("./components/toolbox").then((mod) => mod.Toolbox),
+);
+const ContactSection = dynamic(() =>
+  import("./components/contact-section").then((mod) => mod.ContactSection),
+);
+const Footer = dynamic(() =>
+  import("./components/footer").then((mod) => mod.Footer),
+);
 export default function Home() {
   return (
     <main className="relative bg-background text-foreground overflow-x-hidden">
